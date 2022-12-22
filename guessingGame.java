8c2d0e549ccf2104b32006c6eb0be8ea6cef55fe
Athina <athina.stewart@gmail.com>
2022-12-22 18:29:55 -0500

    init

--- guessingGame.java
+++ guessingGame.java
@@ -1,2 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class guessingGame {
+    static int player1Score;
+    static int player2Score;
+
+    private static void readFile() {
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("wordBank.tx"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        readFile();
+    }
 }
