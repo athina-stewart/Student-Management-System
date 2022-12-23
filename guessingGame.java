5599d879b8a1b11ebdff3836b6097312085176e4
Athina <athina.stewart@gmail.com>
2022-12-22 20:33:44 -0500

    init

--- guessingGame.java
+++ guessingGame.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class guessingGame {
     static int player1Score;
     static int player2Score;
+    static int round = 0;
     static String[] fiveLetterWords = new String[15];
     static String[] sixLetterWords = new String[15];
     static String[] sevenLetterWords = new String[15];
@@ -35,16 +36,60 @@ public class guessingGame {
         }
     }
 
-    private static String getWord(){
-        return("");
+    private static String get5LetterWord(){
+        int theWordsLength = fiveLetterWords.length;
+        int wordIndex = (int)(Math.random() * (theWordsLength));
+        if (fiveLetterWords[wordIndex] == null){
+            return(get5LetterWord());
+        }
+        return(fiveLetterWords[wordIndex]);
+    }
+
+    private static String get6LetterWord(){
+        int theWordsLength = sixLetterWords.length;
+        int wordIndex = (int)(Math.random() * (theWordsLength));
+        if (sixLetterWords[wordIndex] == null){
+            return(get6LetterWord());
+        }
+        return(sixLetterWords[wordIndex]);
+    }
+
+    private static String get7LetterWord(){
+        int theWordsLength = sevenLetterWords.length;
+        int wordIndex = (int)(Math.random() * (theWordsLength));
+        if (sevenLetterWords[wordIndex] == null){
+            return(get7LetterWord());
+        }
+        return(sevenLetterWords[wordIndex]);
     }
 
     private static void playTheGame(){
-
+        while (round < 5){
+            printTheRound();
+            if (round < 3) {
+                String word = get5LetterWord();
+                System.out.println(word);
+                round++;
+            }
+            if (round == 3){
+                printTheRound();
+                round++;
+            }
+            if (round == 4){
+                printTheRound();
+                round++;
+            }
+        }
     }
 
     private static void printTheRound(){
-
+        System.out.println("THIS IS ROUND " + (round+1) + " !\n");
+        if ((round + 1) == 4){
+            System.out.println("DOUBLE POINTS!\n");
+        }
+        if ((round + 1) == 5){
+            System.out.println("TRIPLE POINTS!\n");
+        }
     }
 
     private static void printTheScore(){
@@ -53,5 +98,6 @@ public class guessingGame {
 
     public static void main(String[] args) {
         readFile("/Users/Athina/IdeaProjects/PersonalProjects/src/wordBank.txt");
+        playTheGame();
     }
 }
