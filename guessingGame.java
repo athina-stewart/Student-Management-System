bd2a89b4fc2ff0505d6ca1a6481f1af4553d0713
Athina <athina.stewart@gmail.com>
2022-12-22 19:11:43 -0500

    init

--- guessingGame.java
+++ guessingGame.java
@@ -6,24 +6,25 @@ import java.util.Arrays;
 public class guessingGame {
     static int player1Score;
     static int player2Score;
-    static String[] fiveLetterWords = new String[100000];
-    static String[] sixLetterWords = new String[100000];
-    static String[] sevenLetterWords = new String[100000];
+    static String[] fiveLetterWords = new String[15];
+    static String[] sixLetterWords = new String[15];
+    static String[] sevenLetterWords = new String[15];
 
     private static void readFile(String fileName) {
         try (BufferedReader in = new BufferedReader(new FileReader(fileName))){
             int counter1 = 0;
             int counter2 = 0;
             int counter3 = 0;
-            while (in.readLine() != null){
-                if (in.readLine().length() == 5){
-                    fiveLetterWords[counter1++] = in.readLine();
+            String word;
+            while ((word = in.readLine()) != null){
+                if (word.length() == 5){
+                    fiveLetterWords[counter1++] = word;
                 }
-                if (in.readLine().length() == 6){
-                    sixLetterWords[counter2++] = in.readLine();
+                if (word.length() == 6){
+                    sixLetterWords[counter2++] = word;
                 }
-                if (in.readLine().length() == 7){
-                    sevenLetterWords[counter3++] = in.readLine();
+                if (word.length() == 7){
+                    sevenLetterWords[counter3++] = word;
                 }
             }
             System.out.println(Arrays.toString(fiveLetterWords));
