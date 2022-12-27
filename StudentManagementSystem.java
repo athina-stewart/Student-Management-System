7eb3e5cf495228389bfcd70d8d8dfb507a027149
Athina <athina.stewart@gmail.com>
2022-12-27 18:37:26 -0500

    init

--- StudentManagementSystem.java
+++ StudentManagementSystem.java
@@ -1,4 +1,42 @@
-public class StudentManagementSystem {
+/**
+ * Available commands: add, delete, update, find, exit
+ * **/
 
+import java.util.LinkedList;
+
+public class StudentManagementSystem {
+    LinkedList<StudentRecord> studentList;
+
+    public StudentManagementSystem(){
+        studentList = new LinkedList<>();
+    }
+
+    public boolean find(int studentID){
+        for (StudentRecord studentRecord : studentList) {
+            if (studentID == studentRecord.getSchoolID()) {
+                return true;
+            }
+        }
+        return false;
+//        for (int index = 0; index < studentList.size(); index ++){
+//            if (studentID == studentList.get(index).getSchoolID()){
+//                return true;
+//            }
+//        }
+//        return false;
+    }
+
+    public void add(StudentRecord record){
+        if (find(record.getSchoolID())){
+            System.out.println("Student is already recorded");
+        } else {
+            studentList.add(record);
+            System.out.println("Student has been added to management system");
+        }
+    }
+
+    public void delete(int studentID){
+
+    }
 
 }
